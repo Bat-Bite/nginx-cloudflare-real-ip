6dc7ead5d6fccc6bdcbdb7f8622ef11665978cce
RoeZX <spsandoval94@gmail.com>
2023-02-19 14:30:54 -0300

    primer commit

---
+++ cloudflare-sync-ips-use-api.sh
@@ -0,0 +1,26 @@
+# Establecer lugar donde se guardara el archivo final
+CLOUDFLARE_FILE_PATH=${1:-/etc/nginx/cloudflare}
+
+# Obtener el token de API de Cloudflare
+CLOUDFLARE_API_TOKEN="tu_token_de_API"
+
+# Obtener la lista de direcciones IP de Cloudflare desde la API
+CLOUDFLARE_IPV4_RANGES=$(curl -s -X GET "https://api.cloudflare.com/client/v4/ips" \
+  -H "Authorization: Bearer $CLOUDFLARE_API_TOKEN" \
+  | jq -r '.result.ipv4_cidrs[]')
+
+CLOUDFLARE_IPV6_RANGES=$(curl -s -X GET "https://api.cloudflare.com/client/v4/ips" \
+  -H "Authorization: Bearer $CLOUDFLARE_API_TOKEN" \
+  | jq -r '.result.ipv6_cidrs[]')
+
+# Agregar las direcciones IP a la configuración de Nginx
+echo "# - IPv4" >> $CLOUDFLARE_FILE_PATH;
+for i in $CLOUDFLARE_IPV4_RANGES; do
+  echo "set_real_ip_from $i;" >> $CLOUDFLARE_FILE_PATH;
+done
+
+echo "" >> $CLOUDFLARE_FILE_PATH;
+echo "# - IPv6" >> $CLOUDFLARE_FILE_PATH;
+for i in $CLOUDFLARE_IPV6_RANGES; do
+  echo "set_real_ip_from $i;" >> $CLOUDFLARE_FILE_PATH;
+done
